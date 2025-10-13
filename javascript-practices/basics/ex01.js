2c87e99e29cd694344de6f689e44fa1caf348d53
sung9920 <sungcheol9920@gmail.com>
2025-10-13 11:07:59 +0900

    primitive type

---
+++ javascript-practices/basics/ex01.js
@@ -0,0 +1,14 @@
+console.log("======= [01] 기본타입, primitive type =======");
+var u;
+var i = 10;
+var s = "hello";
+var b = true;
+
+console.log("u:" + typeof(u)); // undefined
+console.log("i:" + typeof(i)); // number
+console.log("s:" + typeof(s)); // string
+console.log("b:" + typeof(b)); // boolean
+
+u = k + 10; // k is not defined
+
+console.log(k);
