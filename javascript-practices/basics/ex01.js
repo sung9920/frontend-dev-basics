105d37e27dc78acdb41044cb77771a1d4a2836bf
sung9920 <sungcheol9920@gmail.com>
2025-10-13 11:56:36 +0900

    function type

--- javascript-practices/basics/ex01.js
+++ javascript-practices/basics/ex01.js
@@ -9,6 +9,26 @@ console.log("i:" + typeof(i)); // number
 console.log("s:" + typeof(s)); // string
 console.log("b:" + typeof(b)); // boolean
 
-u = k + 10; // k is not defined
+console.log("======= [02] 객체타입, object type =======");
+var i = new Number(10);
+var s = new String("hello");
+var b = new Boolean(true);
+var o = {};
+var a = [];
+var n = null;
 
-console.log(k);
+console.log("i:" + typeof(i)); // object
+console.log("s:" + typeof(s)); // object    
+console.log("b:" + typeof(b)); // object
+console.log("o:" + typeof(o)); // object
+console.log("a:" + typeof(a)); // object
+console.log("n:" + typeof(n)); // object
+
+console.log("======= [03] 함수타입, function type =======");
+var f1 = function() {};
+function f2() {}
+var f3 = new Function();
+
+console.log("f1:" + typeof(f1)); // function
+console.log("f2:" + typeof(f2)); // function
+console.log("f3:" + typeof(f3)); // function
